a22c5e72cf1833d14b42db443f8ad7d3e0202285
doga ozcan <dogasava@gmail.com>
2021-05-16 14:09:29 +0300

    Update distributionLock.sol

--- distributionLock.sol
+++ distributionLock.sol
@@ -1,10 +1,13 @@
 pragma solidity 0.5.16;
 
 import './lockClaim.sol';
+import './SafeMath.sol';
 
 contract distributionClaim {
  
     lockClaim private _lockClaim;
+    
+    using SafeMath for uint256;
     
     address private contractOwner;
     
@@ -27,11 +30,45 @@ contract distributionClaim {
         _lockClaim = lockClaim(_lockClaimAddress);
     }
     
+    function transferToContractAll() public payable onlyOwner{
+        
+    }
+    
+      function getRatioClaimAmount (uint256 a, uint256 b, uint256 amount) internal pure returns(uint256){
+         uint256 result = amount.mul(a);
+         return result.div(b);
+    }
+    
     function startDistribution() external {
        
-       bool result=  _lockClaim.isAddressClaimableForDistribution(msg.sender,idoStartTime,idoEndingTime);
+       (bool result,uint256 tierIndex) =  _lockClaim.isAddressClaimableForDistribution(msg.sender,idoStartTime,idoEndingTime);
+       uint256 nestSize = _lockClaim.getNestSize();
        if(result){//dağıtımı hesapla ve gönderim yap
-           
+       
+        address contractAddress = address(this);
+
+        uint256 totalBalance = contractAddress.balance.div(nestSize);
+        
+        uint256 newTotalBalance = totalBalance.div(375);
+        
+         uint256 distributionForTier;
+        
+        if(tierIndex == 1){
+           distributionForTier = getRatioClaimAmount(15,100,newTotalBalance);
+        }
+        
+        else if(tierIndex == 2){
+             distributionForTier = getRatioClaimAmount(35,100,newTotalBalance);
+        }
+        
+        else if(tierIndex == 3){
+            distributionForTier = getRatioClaimAmount(50,100,newTotalBalance);
+        }
+        
+        (bool success, ) =  msg.sender.call.value(distributionForTier)("");
+        
+        
+        require(success, "Transfer failed for startDistribution ");
        }
         
     }
