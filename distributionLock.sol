302258e37044841c1a9b67b22c078425bd93c57f
doga ozcan <dogasava@gmail.com>
2021-05-26 22:10:28 +0300

    Update distributionLock.sol

--- distributionLock.sol
+++ distributionLock.sol
@@ -15,6 +15,12 @@ contract distributionClaim {
     
     lockClaim private _lockClaim;
     
+    uint256 idoCount = 0; // 0 pasif 1 aktif;
+    
+     mapping(address => address) mappingAddressLinkedList;
+     address constant guard = address(1);
+     address[] addressLinkedList;
+    
     modifier onlyOwner() {
         require(contractOwner == msg.sender, "Ownable: caller is not the owner");
         _;
@@ -24,26 +30,67 @@ contract distributionClaim {
          contractOwner = msg.sender;
         _lockClaim = lockClaim(_lockClaimAddress);
     }
+    
+    function ifAddable() internal view returns (bool){
+        if(mappingAddressLinkedList[msg.sender] == address(0)){
+            return true;
+        }
+        else
+         return false;
+         
+     }
+    
+     function addAddress() internal {
+         require(ifAddable(), "cant add" );
+         uint256 len = addressLinkedList.length;
+         if(len == 1){
+              mappingAddressLinkedList[guard] = msg.sender;
+              mappingAddressLinkedList[msg.sender] = guard;
+              addressLinkedList.push(msg.sender);
+             
+         }
+         else{
+             mappingAddressLinkedList[addressLinkedList[len-1]] = msg.sender;
+             mappingAddressLinkedList[msg.sender] = guard;
+             addressLinkedList.push(msg.sender);
+            
+         }
+     }
 
        //dağıtım işlemeri
 
-    function setIdoStartEndTime(uint256 _timestamp) external onlyOwner {
+    function setIdoStart(uint256 _timestamp) external onlyOwner {
         idoStartTime  = block.timestamp;
         idoEndingTime = block.timestamp + _timestamp * 1 days;
+         idoCount = 1;
+        mappingAddressLinkedList[guard] = guard;
+        addressLinkedList.push(guard);
     }
+    
+     function setIdoFinish() external onlyOwner {
+         uint256 i = 0; 
+        do{
+          mappingAddressLinkedList[addressLinkedList[i]] = address(0);
+          i++;  
+        } 
+        while(i<addressLinkedList.length);
+         
+         delete addressLinkedList;
+         mappingAddressLinkedList[guard] = guard;
+         addressLinkedList.push(guard);
+         idoCount = 0;
+    }
+  
     
     //kazanılan tüm bnb bakiyeleri bu metod ile bu sozleşmeye gönderilecektir.
     function transferToContractAll() public payable onlyOwner{
         
     }
     
-     function depositUsingVariable() public payable { 
-
     
-
+     function depositUsingVariable() public payable { 
     }
 
-  
     
     function getRatioClaimAmount (uint256 a, uint256 b, uint256 amount) internal pure returns(uint256){
          uint256 result = amount.mul(a);
@@ -54,19 +101,27 @@ contract distributionClaim {
        return (idoStartTime,idoEndingTime);
     }
     
+    //0 ise ido bitmiştr ve claim yapılamaz uyarısı cıkarılır
+    //1 ise ido devam ediyor claim yapabilirsiniz uyarısı cıkarılır
+    function isIdoTimeOrNot() external view returns(uint256){
+        return idoCount;
+    }
+
+
     //verilen tarihler arası hakeden kişinin adresi hangi tier içerisinde ise 
     // bulunur ve kişinin adresine gönderim yapılır
     function startDistribution() external payable{
-       
+        
         uint256 distributionForTier;
-        address contractAddress = address(this);
-       
+        require(idoCount == 1,"Not Ido Time");
+        addAddress();
+        
         (bool result,uint256 tierIndex) =  _lockClaim.isAddressClaimableForDistribution(msg.sender,idoStartTime,idoEndingTime);
         uint256 nestSize = _lockClaim.getNestSize();
         
         require(result,"need right for claim");//dağıtımı hesapla ve gönderim yap
        
-        uint256 totalBalance = contractAddress.balance.div(nestSize);
+        uint256 totalBalance = address(this).balance.div(nestSize);
         
         uint256 newTotalBalance = totalBalance.div(375);
         
@@ -83,8 +138,6 @@ contract distributionClaim {
         }
         
         msg.sender.transfer(distributionForTier);
-        
-
         
     }
     
